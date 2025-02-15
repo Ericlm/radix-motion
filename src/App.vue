8db4e1fa9c9c536e55ad4013fae5e7840d892ccc
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-02-15 17:14:48 +0100

    🔧 Tailwind

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,7 @@
 </script>
 
 <template>
+  <span>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa praesentium exercitationem quod. Ipsam ab aperiam quas quos, sit laudantium veniam.
+  </span>
 </template>
