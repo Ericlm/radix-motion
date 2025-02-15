25053701cd648b958ed3372c668438ab5f17b133
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-02-15 17:19:00 +0100

    ✨ Popover

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import { Popover } from 'radix-vue/namespaced';
 </script>
 
 <template>
-  <span>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa praesentium exercitationem quod. Ipsam ab aperiam quas quos, sit laudantium veniam.
-  </span>
+  <div class="px-4">
+    <Popover.Root>
+      <Popover.Trigger class="cursor-pointer">
+        Toggle popover
+      </Popover.Trigger>
+      
+      <Popover.Portal>
+        <Popover.Content class="px-4 py-2 bg-pink-400">
+          I'm a popover!
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  </div>
 </template>
